74589451ef503322390602711708ef41ec5d5393
RonsonGallery <57090631+RonsonGallery@users.noreply.github.com>
2023-05-04 17:56:00 +0300

    Added packet sniffer

--- project.py
+++ project.py
@@ -34,25 +34,72 @@ for dst_port in port_range:
         ):
             print(f"{host}:{dst_port} is filtered (silently dropped).")
 """
+from scapy.all import *
 import networkx as nx
 from scapy.all import ARP, Ether, srp
 import matplotlib.pyplot as plt
 import psutil
+import time
+import threading
 
-def Get_Trafic(IP):
-    ip_address = IP
 
-    # Get the network connections for the device with the given IP address
-    connections = psutil.net_connections()
+start_time = time.time()
+time_limit = 10
+clients = []
+counter = 0
+ip_address = ""
 
-    # Find the network connection with the given IP address
-    for conn in connections:
-        if conn.laddr.ip == ip_address:
-            # Get the network traffic statistics for the connection
-            stat = conn.status.lower()
-            if stat == "established":
-                print(f"Sent: {conn.sent}, Received: {conn.recv}")
-                break
+address = []
+
+total_traffic = {}
+
+def capture_traffic(pkt):
+    if IP in pkt:
+        ip = pkt[IP].src
+        if ip not in total_traffic:
+            total_traffic[ip] = 0
+        total_traffic[ip] += pkt[IP].len
+"""
+def capture_traffic(pkt):
+    if IP in pkt and pkt[IP].src in address:
+        ip = pkt[IP].src
+        total_traffic[ip] += pkt[IP].len
+
+
+def stop_sniffing():
+    if sniff_thread and sniff_thread.is_alive():
+        sniff_thread._stop()
+
+"""
+
+"""
+def capture_traffic(pkt):
+    global total_traffic
+    
+    if IP in pkt:
+        for ip in address:
+            if pkt[IP].src == ip or pkt[IP].dst == ip:
+                # Packet is related to one of the IP addresses we're interested in
+                elapsed_time = time.time() - start_time
+                if elapsed_time <= time_limit:
+                    # Only count packets captured within the time limit
+                    total_traffic[ip] += pkt[IP].len
+                else:
+                    # Stop capturing packets after time limit is reached
+                    sniff(prn=lambda x: None, filter="", store=0)
+
+"""
+
+"""
+
+def capture_traffic(pkt, address):
+    if IP in pkt and (pkt[IP].src in address or pkt[IP].dst in address):
+        # Packet is related to one of the IP addresses we're interested in
+        if pkt[IP].src in address:
+            print(f"{pkt[IP].src} sent {pkt[IP].len} bytes")
+        else:
+            print(f"{pkt[IP].dst} received {pkt[IP].len} bytes")
+"""
 
 G = nx.Graph()
 G.add_node("The Internet")
@@ -76,14 +123,14 @@ print(result)
 
 
 # a list of clients, we will fill this in the upcoming loop
-clients = []
-counter = 0
+
 
 for sent, received in result:
     # for each response, append ip and mac address to `clients` list
     clients.append({'ip': received.psrc, 'mac': received.hwsrc})
     if (counter == 0):
         DeafultGateWay = received.psrc
+        address.append(received.psrc)
         G.add_edge("The Internet",received.psrc)
         counter = counter + 1
 
@@ -91,9 +138,32 @@ for sent, received in result:
     G.add_node(received.psrc)
     if(DeafultGateWay != received.psrc):
         G.add_edge(DeafultGateWay,received.psrc)
+        address.append(received.psrc)
+        total_traffic[received.psrc] = 0
     
-    Get_Trafic(received.psrc)
-    
+    #Get_Trafic(received.psrc)
+"""
+# Start a new thread to handle the timer
+timer_thread = threading.Thread(target=stop_sniffing)
+timer_thread.start()
+
+# Start sniffing for traffic
+sniff_thread = threading.Thread(target=sniff, kwargs={"prn": capture_traffic, "filter": "ip"})
+sniff_thread.start()
+
+# Wait for the sniffing thread to finish
+sniff_thread.join()
+
+# Print total traffic for each IP address
+for ip in address:
+    print(f"Total traffic for {ip}: {total_traffic[ip]} bytes")
+"""
+
+sniff(prn=capture_traffic, filter="ip", timeout= time_limit)
+for ip in address:
+    print(f"Total traffic for {ip} in the last {time_limit} seconds: {total_traffic[ip]/1024} Mega bytes")
+#sniff(prn=lambda pkt: capture_traffic(pkt, address), filter="ip")
+
 
 
 # print clients
