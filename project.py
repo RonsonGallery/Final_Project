c60cac846fceb586865f9b8e52022eaaad048a10
RonsonGallery <57090631+RonsonGallery@users.noreply.github.com>
2023-05-05 12:18:12 +0300

    Removed code that was not usefull anymore

--- project.py
+++ project.py
@@ -38,9 +38,7 @@ from scapy.all import *
 import networkx as nx
 from scapy.all import ARP, Ether, srp
 import matplotlib.pyplot as plt
-import psutil
 import time
-import threading
 
 
 start_time = time.time()
@@ -59,47 +57,6 @@ def capture_traffic(pkt):
         if ip not in total_traffic:
             total_traffic[ip] = 0
         total_traffic[ip] += pkt[IP].len
-"""
-def capture_traffic(pkt):
-    if IP in pkt and pkt[IP].src in address:
-        ip = pkt[IP].src
-        total_traffic[ip] += pkt[IP].len
-
-
-def stop_sniffing():
-    if sniff_thread and sniff_thread.is_alive():
-        sniff_thread._stop()
-
-"""
-
-"""
-def capture_traffic(pkt):
-    global total_traffic
-    
-    if IP in pkt:
-        for ip in address:
-            if pkt[IP].src == ip or pkt[IP].dst == ip:
-                # Packet is related to one of the IP addresses we're interested in
-                elapsed_time = time.time() - start_time
-                if elapsed_time <= time_limit:
-                    # Only count packets captured within the time limit
-                    total_traffic[ip] += pkt[IP].len
-                else:
-                    # Stop capturing packets after time limit is reached
-                    sniff(prn=lambda x: None, filter="", store=0)
-
-"""
-
-"""
-
-def capture_traffic(pkt, address):
-    if IP in pkt and (pkt[IP].src in address or pkt[IP].dst in address):
-        # Packet is related to one of the IP addresses we're interested in
-        if pkt[IP].src in address:
-            print(f"{pkt[IP].src} sent {pkt[IP].len} bytes")
-        else:
-            print(f"{pkt[IP].dst} received {pkt[IP].len} bytes")
-"""
 
 G = nx.Graph()
 G.add_node("The Internet")
@@ -109,8 +66,12 @@ target_ip = "192.168.0.0/24"
 # IP Address for the destination
 # create ARP packet
 arp = ARP(pdst=target_ip)
+
+
 # create the Ether broadcast packet
 # ff:ff:ff:ff:ff:ff MAC address indicates broadcasting
+
+
 ether = Ether(dst="ff:ff:ff:ff:ff:ff")
 # stack them
 packet = ether/arp
@@ -142,28 +103,11 @@ for sent, received in result:
         total_traffic[received.psrc] = 0
     
     #Get_Trafic(received.psrc)
-"""
-# Start a new thread to handle the timer
-timer_thread = threading.Thread(target=stop_sniffing)
-timer_thread.start()
 
-# Start sniffing for traffic
-sniff_thread = threading.Thread(target=sniff, kwargs={"prn": capture_traffic, "filter": "ip"})
-sniff_thread.start()
-
-# Wait for the sniffing thread to finish
-sniff_thread.join()
-
-# Print total traffic for each IP address
-for ip in address:
-    print(f"Total traffic for {ip}: {total_traffic[ip]} bytes")
-"""
 
 sniff(prn=capture_traffic, filter="ip", timeout= time_limit)
 for ip in address:
     print(f"Total traffic for {ip} in the last {time_limit} seconds: {total_traffic[ip]/1024} Mega bytes")
-#sniff(prn=lambda pkt: capture_traffic(pkt, address), filter="ip")
-
 
 
 # print clients
